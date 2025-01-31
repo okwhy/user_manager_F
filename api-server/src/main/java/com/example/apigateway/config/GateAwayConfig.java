dbb86911c1117c3573b2f3b69581d2dd2eb69e5c
121Artem122 <99825505+121Artem122@users.noreply.github.com>
2025-01-31 21:11:56 +0400

    Add files via upload

---
+++ api-server/src/main/java/com/example/apigateway/config/GateAwayConfig.java
@@ -0,0 +1,11 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class GateAwayConfig {
+//    public RouteLocater customRouteLocater()
+//    {
+//
+//    }
+//}
